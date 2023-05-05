4807d3d121af4c8e261d419599dedd2e2468b0a0
james <jameschapman17@gmail.com>
2023-05-05 13:38:18 -0700

    Initial Commit; :]

---
+++ Happy_Games.py
@@ -0,0 +1,85 @@
+class Individual:
+	def __init__(self):
+		pass
+
+
+"""
+
+Goals:
+Show that humans are happier if we collectively reduce hours and compensate for each other. 
+Encourage Research
+Encourage Automation
+Show that a Basic Universal Income makes people happier
+
+
+
+C
+	Tighter Knit Social Groups
+
+G
+L
+S
+	More Natural Environments (Not Cubicals)
+P
+
+Autonomy:
+	Freedom to pursue Interest
+	Generally not being told what to do or how to do it
+
+Pain
+
+Health
+
+Safety
+
+Protection from the elements
+
+Access to Food and Water
+
+Other Priorities in some order having to do with possessions
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
+
